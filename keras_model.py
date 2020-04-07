0c4c017fb8833c031f028f135ee9b1fb9555c4ef
yui0204 <yui.sudo@gmail.com>
2020-04-07 17:15:59 +0900

    added ASPP module

--- keras_model.py
+++ keras_model.py
@@ -17,12 +17,111 @@ keras.backend.set_image_data_format('channels_first')
 from IPython import embed
 import numpy as np
 
+from keras.engine import Layer
+from keras import backend as K
+from keras.engine import InputSpec
+from keras.utils import conv_utils
+from keras.layers import AveragePooling2D
+from keras.layers import DepthwiseConv2D
+from keras.layers import ZeroPadding2D
+
+class BilinearUpsampling(Layer):
+    """Just a simple bilinear upsampling layer. Works only with TF.
+       Args:
+           upsampling: tuple of 2 numbers > 0. The upsampling ratio for h and w
+           output_size: used instead of upsampling arg if passed!
+    """
+
+    def __init__(self, upsampling=(2, 2), output_size=None, data_format=None, **kwargs):
+
+        super(BilinearUpsampling, self).__init__(**kwargs)
+
+        self.data_format = conv_utils.normalize_data_format(data_format)
+#        self.data_format = K.normalize_data_format(data_format)
+        self.input_spec = InputSpec(ndim=4)
+        if output_size:
+            self.output_size = conv_utils.normalize_tuple(
+                output_size, 2, 'output_size')
+            self.upsampling = None
+        else:
+            self.output_size = None
+            self.upsampling = conv_utils.normalize_tuple(upsampling, 2, 'upsampling')
+
+    def compute_output_shape(self, input_shape):
+        if self.upsampling:
+            height = self.upsampling[0] * \
+                input_shape[1] if input_shape[1] is not None else None
+            width = self.upsampling[1] * \
+                input_shape[2] if input_shape[2] is not None else None
+        else:
+            height = self.output_size[0]
+            width = self.output_size[1]
+        return (input_shape[0], height, width, input_shape[3])
+
+    def call(self, inputs):
+        if self.upsampling:
+            return K.tf.image.resize_bilinear(inputs, (inputs.shape[1] * self.upsampling[0],
+                                                       inputs.shape[2] * self.upsampling[1]),
+                                              align_corners=True)
+        else:
+            return K.tf.image.resize_bilinear(inputs, (self.output_size[0],
+                                                       self.output_size[1]),
+                                              align_corners=True)
+
+    def get_config(self):
+        config = {'upsampling':self.upsampling,
+                  'output_size': self.output_size,
+                  'data_format': self.data_format}
+        base_config = super(BilinearUpsampling, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+    
+
+def SepConv_BN(x, filters, prefix, stride=1, kernel_size=3, rate=1, depth_activation=False, epsilon=1e-3):
+    """ SepConv with BN between depthwise & pointwise. Optionally add activation after BN
+        Implements right "same" padding for even kernel sizes
+        Args:
+            x: input tensor
+            filters: num of filters in pointwise convolution
+            prefix: prefix before name
+            stride: stride at depthwise conv
+            kernel_size: kernel size for depthwise convolution
+            rate: atrous rate for depthwise convolution
+            depth_activation: flag to use activation between depthwise & poinwise convs
+            epsilon: epsilon to use in BN layer
+    """
+
+    if stride == 1:
+        depth_padding = 'same'
+    else:
+        kernel_size_effective = kernel_size + (kernel_size - 1) * (rate - 1)
+        pad_total = kernel_size_effective - 1
+        pad_beg = pad_total // 2
+        pad_end = pad_total - pad_beg
+        x = ZeroPadding2D((pad_beg, pad_end))(x)
+        depth_padding = 'valid'
+
+    if not depth_activation:
+        x = Activation('relu')(x)
+    x = DepthwiseConv2D((kernel_size, kernel_size), strides=(stride, stride), dilation_rate=(rate, rate),
+                        padding=depth_padding, use_bias=False, name=prefix + '_depthwise')(x)
+    x = BatchNormalization(name=prefix + '_depthwise_BN', epsilon=epsilon)(x)
+    if depth_activation:
+        x = Activation('relu')(x)
+    x = Conv2D(filters, (1, 1), padding='same',
+               use_bias=False, name=prefix + '_pointwise')(x)
+    x = BatchNormalization(name=prefix + '_pointwise_BN', epsilon=epsilon)(x)
+    if depth_activation:
+        x = Activation('relu')(x)
+
+    return x
+
+
 
 def get_model(data_in, data_out, dropout_rate, nb_cnn2d_filt, f_pool_size, t_pool_size,
               rnn_size, fnn_size, weights, doa_objective):
     # model definition
     spec_start = Input(shape=(data_in[-3], data_in[-2], data_in[-1]))
-
+    print(spec_start)
     # CNN
     spec_cnn = spec_start
     for i, convCnt in enumerate(f_pool_size):
@@ -31,10 +130,40 @@ def get_model(data_in, data_out, dropout_rate, nb_cnn2d_filt, f_pool_size, t_poo
         spec_cnn = Activation('relu')(spec_cnn)
         spec_cnn = MaxPooling2D(pool_size=(t_pool_size[i], f_pool_size[i]))(spec_cnn)
         spec_cnn = Dropout(dropout_rate)(spec_cnn)
-    spec_cnn = Permute((2, 1, 3))(spec_cnn)
 
+
+    # ASPP module
+    atrous_rates = (6, 12, 18)
+    b0 = Conv2D(64, (1, 1), padding='same', use_bias=False, name='aspp0')(spec_cnn)
+    b0 = BatchNormalization(name='aspp0_BN', epsilon=1e-5)(b0)
+    b0 = Activation('relu', name='aspp0_activation')(b0)
+    # rate = 6 (12)
+    b1 = SepConv_BN(spec_cnn, 64, 'aspp1',
+                    rate=atrous_rates[0], depth_activation=True, epsilon=1e-5)
+    # rate = 12 (24)
+    b2 = SepConv_BN(spec_cnn, 64, 'aspp2',
+                    rate=atrous_rates[1], depth_activation=True, epsilon=1e-5)
+    # rate = 18 (36)
+    b3 = SepConv_BN(spec_cnn, 64, 'aspp3',
+                    rate=atrous_rates[2], depth_activation=True, epsilon=1e-5)
+    # Image Feature branch
+    out_shape = int(np.ceil(data_in[-3] / 16))
+    b4 = AveragePooling2D(pool_size=(out_shape, out_shape))(spec_cnn)
+    b4 = Conv2D(64, (1, 1), padding='same',
+                use_bias=False, name='image_pooling')(b4)
+    b4 = BatchNormalization(name='image_pooling_BN', epsilon=1e-5)(b4)
+    b4 = Activation('relu')(b4)
+    b4 = BilinearUpsampling((out_shape, out_shape))(b4)
+
+    # concatenate ASPP branches & project
+    spec_cnn = Concatenate()([b4, b0, b1, b2, b3])
+    print(spec_cnn)
+    
+    
+    spec_cnn = Permute((2, 1, 3))(spec_cnn)
     # RNN
     spec_rnn = Reshape((data_out[0][-2], -1))(spec_cnn)
+    print("before RNN", spec_rnn)
     for nb_rnn_filt in rnn_size:
         spec_rnn = Bidirectional(
             GRU(nb_rnn_filt, activation='tanh', dropout=dropout_rate, recurrent_dropout=dropout_rate,
